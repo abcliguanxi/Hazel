661e5218fb8635fefb603f1faeb081c28d3d6a9e
ligx <liguanxi@zeewain.com>
2024-09-13 11:09:50 +0800

    fixed:修复由于预编译头没有包含Hazel/Log.h出现在Sandbox项目中不能调用HZ_CORE_WARN的问题

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,9 @@ extern Hazel::Application* Hazel::CreateApplication();//在某个地方定义的函数会返
 int main(int argc,char** argv) {
 
 	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
--- Hazel/src/hzpch.h
+++ Hazel/src/hzpch.h
@@ -6,11 +6,13 @@
 #include <algorithm>
 #include <functional>
 
+#include <string>
 #include <sstream>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
 
+#include "Hazel/Log.h"
 
 #ifdef HZ_PLATFORM_WINDOWS
 	#include <Windows.h>
