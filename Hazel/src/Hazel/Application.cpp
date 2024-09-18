3299c81081c721002a786121a3f8f82e62229bdb
ligx <liguanxi@zeewain.com>
2024-09-18 16:55:03 +0800

    Added Hazel key and mouse codes.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -67,12 +67,7 @@ namespace Hazel {
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			for (Layer* layer : m_LayerStack)
-			{
 				layer->OnUpdate();
-			}
-
-			auto [x, y] = Input::GetMousePosition();
-			HZ_CORE_TRACE("{0},{1}", x, y);
 
 			m_Window->OnUpdate();
 		}
