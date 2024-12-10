5bd4b09b43a5254766b74f71eba7e7f10379912f
ligx <liguanxi@zeewain.com>
2024-12-10 18:57:42 +0800

    Added Internal Calls + some refactoring

--- Hazel/src/Hazel/Scripting/ScriptEngine.h
+++ Hazel/src/Hazel/Scripting/ScriptEngine.h
@@ -1,5 +1,15 @@
 ﻿#pragma once
 
+#include <filesystem>
+#include <string>
+
+//这段代码告诉编译器有MonoClass的定义不要报错,真正在cpp文件中 能够在mono找到对应的定义
+extern "C" {//指示编译器这部分的代码按C语言，而不是C++的方式进行编译。
+	typedef struct _MonoClass MonoClass;
+	typedef struct _MonoObject MonoObject;
+	typedef struct _MonoMethod MonoMethod;
+}
+
 namespace Hazel{
 
 	class ScriptEngine
@@ -7,9 +17,31 @@ namespace Hazel{
 	public:
 		static void Init();
 		static void Shutdown();
+
+		static void LoadAssembly(const std::filesystem::path& filepath);
 	private:
 		static void InitMono();
 		static void ShutdownMono();
+
+		static MonoObject* InstantiateClass(MonoClass* monoClass);
+
+		friend class ScriptClass;
+	};
+
+	class ScriptClass
+	{
+	public:
+		ScriptClass() = default;
+		ScriptClass(const std::string& classNamespace, const std::string& className);
+
+		MonoObject* Instantiate();
+		MonoMethod* GetMethod(const std::string& name, int parameterCount);
+		MonoObject* InvokeMethod(MonoObject* instance, MonoMethod* method, void** params = nullptr);
+	private:
+		std::string m_ClassNamespace;
+		std::string m_ClassName;
+
+		MonoClass* m_MonoClass = nullptr;
 	};
 
 }
---
+++ Hazel/src/Hazel/Scripting/ScriptGlue.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace Hazel {
+
+	class ScriptGlue
+	{
+	public:
+		static void RegisterFunctions();
+	};
+
+
+
+}
